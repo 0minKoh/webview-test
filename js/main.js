97e460eee0f98bb9bb7f16d8f106a94206fb2820
0minKoh <96913649+0minKoh@users.noreply.github.com>
2023-08-01 15:08:37 +0900

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-const form = document.querySelector("form");
-form.addEventListener("submit", () => {
-  alert("Submit");
+const input = document.querySelector("#submit");
+form.addEventListener("click", () => {
+  window.location.reload();
 });
